f41c969c4efd1e336ccdad18d8662aaf7ce5b75b
Jennifer Olsen <jenny@ninthdaydesign.com>
2013-09-04 19:40:09 -0700

    Install postgres

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 group :development do 
-  gem 'sqlite3', '1.3.5'
+  gem 'pg'
   gem 'rspec-rails', '2.9.0'
 end  
 
@@ -29,7 +29,7 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '0.12.2'
+  gem 'pg'
 end    
 
 # To use ActiveModel has_secure_password
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
@@ -11,7 +12,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "#{base_title} | Home")
     end  
   end
   
@@ -24,7 +25,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Help")
+        :text => "#{base_title} | Help")
     end  
   end
 
@@ -37,7 +38,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | About Us")
+        :text => "#{base_title} | About Us")
     end  
   end  
 
@@ -50,7 +51,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Contact")
+        :text => "#{base_title} | Contact")
     end
   end           
 
